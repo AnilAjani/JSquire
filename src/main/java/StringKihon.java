4ed95d12276eae0b69cbd3ee08e23643fdef44e9
Anil Ajani <Anil.Ajani@improving.com>
2019-09-16 14:05:00 -0500

    drill run

--- src/main/java/StringKihon.java
+++ src/main/java/StringKihon.java
@@ -1,61 +1,73 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import framework.StringKihonBase;
+import org.w3c.dom.ls.LSOutput;
 
 public class StringKihon extends StringKihonBase {
 
     @Override
     protected String convertToUpperCase(String data) {
-        throw new UnsupportedOperationException();
+        return data.toUpperCase();
     }
 
     @Override
     protected String convertToLowerCase(String data) {
-        throw new UnsupportedOperationException();
+        return data.toLowerCase();
     }
 
     @Override
     protected String Combine_Parts_Of_A_Name(String firstName, String middleName, String lastName) {
-        throw new UnsupportedOperationException();
+        return first + " " + middle + " " + last;
     }
 
     @Override
-    protected String Combine_Two_Strings(String a, String b) {
-        throw new UnsupportedOperationException();
+    protected String Combine_Two_Strings(String a, String {
+        return a + b;
+
+
+
+
     }
 
     @Override
-    protected int Determine_The_Length_Of_A_String(String data) {
-        throw new UnsupportedOperationException();
+    protected void Determine_The_Length_Of_A_String(String data) {
+
+        return data.length();
     }
 
     @Override
     protected String Remove_All_Leading_Whitespace(String data) {
-        throw new UnsupportedOperationException();
+
+        return data.trim();
     }
+
 
     @Override
     protected String Remove_All_Trailing_Whitespace(String data) {
-        throw new UnsupportedOperationException();
+
+        return data.trim();
     }
 
     @Override
     protected String[] Split_A_String_Into_An_Array(String input, char divider) {
-        throw new UnsupportedOperationException();
+
+        return input.split(String.valueOfdivider)
     }
 
     @Override
     protected String Join_An_Array_Into_A_String(String[] input, String divider) {
-        throw new UnsupportedOperationException();
+
+        return
     }
 
     @Override
     protected boolean returnTrueIfAContainsB(String a, String b) {
-        throw new UnsupportedOperationException();
+        returnTrueIfAContainsB();
     }
 
     @Override
     protected int determineThePositionOfAInB(String a, String b) {
+
         throw new UnsupportedOperationException();
     }
 
@@ -66,11 +78,12 @@ public class StringKihon extends StringKihonBase {
 
     @Override
     protected boolean returnTrueIsAEndsWithB(String a, String b) {
-        throw new UnsupportedOperationException();
+
+        return a.
     }
 
     @Override
     protected String returnTheFourthThroughSeventhCharactersOfInput(String input) {
-        throw new UnsupportedOperationException();
+        input.substring(4,7);
     }
 }
